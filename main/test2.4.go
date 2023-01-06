dccee9c8d7fc28933b799ce88fe82493a3ce73b6
zhangchuntao <zhangchuntao@liangjiangtech.com>
2023-01-06 16:48:02 +0800

    test

---
+++ main/test2.4.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func addition(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(addition(2, 4))
+}
---
+++ main/test2.5.go
@@ -0,0 +1,10 @@
+package main
+
+func addition(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	// var s string = "three"
+	// fmt.Println(addition(2, s))
+}
