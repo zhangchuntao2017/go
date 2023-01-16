e1e9d58863499ecb25a08653eef9521b0176182c
zhangchuntao <zhangchuntao@liangjiangtech.com>
2023-01-16 10:23:34 +0800

    test

---
+++ test8.1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Moive struct {
+	Name   string
+	Rating float64
+}
+
+func (m *Moive) summary() string {
+	r := strconv.FormatFloat(m.Rating, 'f', 1, 64)
+	return m.Name + ", " + r
+}
+
+func main() {
+
+	m := Moive{
+		Name: "Spiderman",
+		Rating: 3.2,
+	}
+
+	fmt.Println(m.summary())
+}
