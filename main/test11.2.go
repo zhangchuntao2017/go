47be35efbae920275f9b9e8516e71c69e914b8bd
zhangchuntao <zhangchuntao@liangjiangtech.com>
2023-01-16 15:23:43 +0800

    test

---
+++ main/test11.2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func slowFunc() {
+	time.Sleep(time.Second * 2)
+	fmt.Println("sleeper() finished")
+}
+
+func main() {
+	go slowFunc()
+	fmt.Println("I am now shown straightaway!")
+
+}
---
+++ main/test11.3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func slowFunc() {
+	time.Sleep(time.Second * 2)
+	fmt.Println("sleeper() finished")
+}
+
+func main() {
+	go slowFunc()
+	fmt.Println("I am now shown straightaway!")
+	time.Sleep(time.Second * 3)
+
+}
